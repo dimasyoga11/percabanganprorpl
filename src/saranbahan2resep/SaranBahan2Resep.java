771f18ea075a9ccf602a51b99fa314e638bb5055
DYPe <DYPe@192.168.137.1>
2020-12-11 10:35:12 +0700

    Tambah Input Bahan 2

--- src/saranbahan2resep/SaranBahan2Resep.java
+++ src/saranbahan2resep/SaranBahan2Resep.java
@@ -28,6 +28,27 @@ public class SaranBahan2Resep {
         System.out.println("2. Telur");
         System.out.println("Masukkan no pilihan Anda :");
         int bahan1 = scanner.nextInt();
+    if(bahan1 == 1 || bahan1 == 2)
+    {
+        System.out.println("Bahan kedua");
+        if(bahan1 == 1) {
+            System.out.println("1. Susu");
+            System.out.println("2. Minyak goreng");
+            System.out.println("3. Tidak ada");
+        }else{
+            System.out.println("1. Minyak goreng");
+            System.out.println("2. Roti");
+            System.out.println("3. Tidak ada");
+        }
+        System.out.print("Masukkan no pilihan Anda :");
+        int bahan2 = scanner.nextInt();
+        
+        
+    }else
+            System.out.println("Mohon maaf, Pilihan tidak dapat ditemukan, "
+                    + "tidak dapat memberikan nama resep");
+                
+    }    
     }
     
 }
